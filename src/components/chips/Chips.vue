c061e459677cc519f8ad92cf704d3237681afdd3
Indra Rudianto <indrarudianto.official@gmail.com>
2021-11-14 09:33:35 +0700

    Add Chips component

---
+++ src/components/chips/Chips.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="chips" :class="classNames" @click="emitClick">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Chips',
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    value: {
+      type: Number,
+    },
+  },
+  computed: {
+    classNames() {
+      return {
+        active: this.active,
+      }
+    },
+  },
+  methods: {
+    emitClick() {
+      if (this.value) {
+        this.$emit('click', this.value)
+      } else {
+        this.$emit('click')
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.chips {
+  padding: 5px;
+  color: #6a6a6a;
+  border: 1px solid #eaeaea;
+  border-radius: 5px;
+  display: inline-block;
+  padding: 0.25rem 0.5rem;
+  font-size: 0.875rem;
+  line-height: 1.5;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+
+  &.active {
+    background-color: #e3e3e3;
+  }
+}
+</style>
