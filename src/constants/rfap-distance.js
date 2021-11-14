48c3986c9a7c5cd5319d0e1d032321db8dfb3eb1
Indra Rudianto <indrarudianto.official@gmail.com>
2021-11-14 09:33:45 +0700

    Add RfapTypes constant

--- src/constants/rfap-distance.js
+++ src/constants/rfap-distance.js
@@ -2,3 +2,14 @@ export const Sampling = Object.freeze({
   DAY: 'day',
   HOUR: 'hour',
 })
+
+/**
+ * RF & AP type identifier.
+ *
+ * Source: https://bma.cendana15.com/docs/apis/monitoring/rfap_type.html
+ */
+export const RfapTypes = Object.freeze({
+  seen: 1,
+  heard: 2,
+  seenAndHeard: 3,
+})
