ba0a9179016a09304b8fb45c044340e3d77f809f
Indra Rudianto <indrarudianto.official@gmail.com>
2021-12-10 09:55:47 +0700

    Use regular component on home view

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import HomeView from '@/views/home/HomeView'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    component: () => import('../views/home/HomeView'),
+    component: HomeView,
   },
   {
     path: '/layout',
