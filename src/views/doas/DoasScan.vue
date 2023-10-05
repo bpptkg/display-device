2484a37621db6ac952b8ff56566ea2b99f97fa7b
Indra Rudianto <indrarudianto.official@gmail.com>
2023-10-05 15:02:56 +0700

    Implement range selector

--- src/views/doas/DoasScan.vue
+++ src/views/doas/DoasScan.vue
@@ -1,7 +1,23 @@
 <template>
   <div style="margin-top: 60px">
-    <BFormSelect v-model="station" :options="stationOptions"></BFormSelect>
-    <DChart :options="option" class="chart"></DChart>
+    <div class="d-flex align-items-center ml-3">
+      <RangeSelector
+        ref="range-selector"
+        size="sm"
+        custom-enabled
+        :selected="period"
+        :items="rangeSelector"
+        :max-custom-duration="maxCustomDuration"
+        @period-selected="onPeriodChange"
+      />
+      <BFormSelect
+        class="ml-3 col-2"
+        size="sm"
+        v-model="station"
+        :options="stationOptions"
+      ></BFormSelect>
+    </div>
+    <DChart ref="chart" :options="option" class="chart"></DChart>
   </div>
 </template>
 
@@ -10,14 +26,19 @@ import { BFormSelect } from 'bootstrap-vue'
 import DChart from '@/components/echarts/chart/DChart'
 import client from '@/utils/client'
 import { toUnixMiliSeconds } from '@/utils/series'
+import RangeSelector from '@/components/range-selector'
+import moment from 'moment'
 
 export default {
   components: {
     DChart,
     BFormSelect,
+    RangeSelector,
   },
   data() {
     return {
+      start: moment().subtract(7, 'days'),
+      end: moment(),
       data: [],
       station: 'jrakah',
       stationOptions: [
@@ -34,6 +55,42 @@ export default {
           text: 'Kaliurang',
         },
       ],
+      period: {
+        count: 7,
+        type: 'day',
+        text: '7 days',
+      },
+      rangeSelector: [
+        {
+          count: 7,
+          type: 'day',
+          text: '7 days',
+        },
+        {
+          count: 1,
+          type: 'month',
+          text: '1 month',
+        },
+        {
+          count: 3,
+          type: 'month',
+          text: '3 months',
+        },
+        {
+          count: 6,
+          type: 'month',
+          text: '6 months',
+        },
+        {
+          count: 1,
+          type: 'year',
+          text: '1 year',
+        },
+      ],
+      maxCustomDuration: {
+        count: 4,
+        type: 'years',
+      },
     }
   },
   computed: {
@@ -59,18 +116,31 @@ export default {
   },
   methods: {
     async fetchData() {
+      const chart = this.$refs.chart.$refs.chart
+      chart.showLoading()
+
       client
         .get('/doas-scan/', {
           params: {
-            start: '2023-09-01 10:00:00',
-            end: '2023-09-01 11:00:00',
+            start: this.start.format('YYYY-MM-DD HH:mm:ss'),
+            end: this.end.format('YYYY-MM-DD HH:mm:ss'),
             station: this.station,
           },
         })
         .then((response) => {
           this.data = response.data
-          console.log(this.data)
         })
+        .catch((error) => {
+          alert('Reqest failed. Please go home.')
+        })
+        .finally(() => {
+          chart.hideLoading()
+        })
+    },
+    onPeriodChange(period, { startTime, endTime }) {
+      this.start = startTime
+      this.end = endTime
+      this.fetchData()
     },
   },
   mounted() {
