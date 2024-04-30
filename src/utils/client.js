d008dcde241457965ab7c0fad184b219a3469d4a
Indra Rudianto <indrarudianto.official@gmail.com>
2024-04-30 10:04:43 +0700

    Remove default timeout

--- src/utils/client.js
+++ src/utils/client.js
@@ -3,14 +3,13 @@ import Axios from 'axios'
 export const API_KEY = process.env.VUE_APP_BMA_API_KEY || ''
 export const ACCESS_TOKEN = process.env.VUE_APP_BMA_ACCESS_TOKEN || ''
 export const BMA_URL = process.env.VUE_APP_BMA_URL || ''
-const defaultTimeout = 15000
+export const defaultTimeout = 15000
 
 const client = Axios.create({
   baseURL: `${BMA_URL}/api/v1/`,
   headers: {
     Authorization: `Api-Key ${API_KEY}`,
   },
-  timeout: defaultTimeout,
 })
 
 export const BaseApiKeyClient = Axios.create({
@@ -18,7 +17,6 @@ export const BaseApiKeyClient = Axios.create({
   headers: {
     Authorization: `Api-Key ${API_KEY}`,
   },
-  timeout: defaultTimeout,
 })
 
 export const BaseOauth2Client = Axios.create({
@@ -26,21 +24,18 @@ export const BaseOauth2Client = Axios.create({
   headers: {
     Authorization: `Bearer ${ACCESS_TOKEN}`,
   },
-  timeout: defaultTimeout,
 })
 
 export const ApiKeyBasedClient = Axios.create({
   headers: {
     Authorization: `Api-Key ${API_KEY}`,
   },
-  timeout: defaultTimeout,
 })
 
 export const OAuth2BasedClient = Axios.create({
   headers: {
     Authorization: `Bearer ${ACCESS_TOKEN}`,
   },
-  timeout: defaultTimeout,
 })
 
 export default client
