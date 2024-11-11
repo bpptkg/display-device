763bd78b4362f619faae5e48173b5798836f1871
Indra Rudianto <indrarudianto.official@gmail.com>
2024-11-11 10:37:53 +0700

    Change helicorder to MEPSL

--- src/store/home/charts/helicorder.js
+++ src/store/home/charts/helicorder.js
@@ -7,5 +7,11 @@ export default {
     [HelicorderChannel.MEPAS_HHZ_VG_00]: initModule(
       HelicorderChannel.MEPAS_HHZ_VG_00
     ),
+    [HelicorderChannel.MELAB_HHZ_VG_00]: initModule(
+      HelicorderChannel.MELAB_HHZ_VG_00
+    ),
+    [HelicorderChannel.MEPSL_HHZ_VG_00]: initModule(
+      HelicorderChannel.MEPSL_HHZ_VG_00
+    ),
   },
 }
